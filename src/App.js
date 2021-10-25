45b7f5eda17aef01ad5777ed848b651e2c4530a9
Zafar Irshad <MY_NAME@ayshasawaab@gmail.com>
2021-10-25 07:41:57 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="head">
+         <h1>THis is header</h1>
+      </div>
+
+      <div className='bodys'>
+        <h1>This is body1</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+        <h1>This is body</h1>
+      </div>
+
+
+      <div className='footer'>
+        <h1>this is footer</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
